1d5471e8c385a4b07c1b1783f723f6ceb8948e28
PacelliV <flores.eugenio03@gmail.com>
2023-04-15 18:54:41 -0500

    completed interface

--- contracts/interfaces/IFelinaToken.sol
+++ contracts/interfaces/IFelinaToken.sol
@@ -3,17 +3,14 @@ pragma solidity ^0.8.0;
 
 /**
  * Interfaces:
- * 1. All must function must be mark as external.
+ * 1. All functions must be declared with an external visibility.
  * 2. Cannot declare state variables.
- * 3. Cannot have a contructor.
- * 4. The function cannot have any implementation.
+ * 3. Cannot declare a contructor.
+ * 4. The functions cannot have any implementation.
  * 5. Can inherit from another interfaces.
  */
 
 interface IFelinaToken {
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
     function transfer(address _to, uint256 _value) external returns (bool success);
 
     function transferFrom(
@@ -24,9 +21,35 @@ interface IFelinaToken {
 
     function approve(address _spender, uint256 _value) external returns (bool success);
 
+    function burn(uint256 _value) external returns (bool success);
+
+    function burnFrom(address _from, uint256 _value) external returns (bool success);
+
+    function setBlockReward(uint256 _blockReward) external returns (bool success);
+
+    function increaseAllowance(
+        address _spender,
+        uint256 _addedValue
+    ) external returns (bool success);
+
+    function decreaseAllowance(
+        address _spender,
+        uint256 _subtractedValue
+    ) external returns (bool success);
+
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
     function totalSupply() external view returns (uint256);
 
     function balanceOf(address _account) external view returns (uint256 balance);
 
     function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+
+    function targetSupply() external view returns (uint256);
+
+    function blockReward() external view returns (uint256);
+
+    function decimals() external pure returns (uint8);
 }
